83ff514bc9aefed9e796ad9d0d97a5d317f65e90
dcharmon <dcorbinharmon@gmail.com>
2026-04-12 15:22:37 -0500

    Add Swagger UI for API documentation

--- src/main/java/edu/matc/inventory/rest/InventoryApplication.java
+++ src/main/java/edu/matc/inventory/rest/InventoryApplication.java
@@ -1,7 +1,13 @@
 package edu.matc.inventory.rest;
 
+import io.swagger.v3.jaxrs2.integration.resources.OpenApiResource;
+import io.swagger.v3.oas.integration.SwaggerConfiguration;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
+import java.util.Collections;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -13,6 +19,31 @@ public class InventoryApplication extends Application {
         Set<Class<?>> classes = new HashSet<>();
         classes.add(InventoryService.class);
         classes.add(ObjectMapperContextResolver.class);
+        classes.add(OpenApiResource.class);
         return classes;
+    }
+
+    @Override
+    public Set<Object> getSingletons() {
+        OpenAPI openAPI = new OpenAPI()
+                .info(new Info()
+                        .title("Fo76 Inventory API")
+                        .version("1.0")
+                        .description("API for managing Fallout 76 player inventories"));
+
+        SwaggerConfiguration config = new SwaggerConfiguration()
+                .openAPI(openAPI)
+                .prettyPrint(true)
+                .resourcePackages(Collections.singleton("edu.matc.inventory.rest"));
+
+        try {
+            new io.swagger.v3.jaxrs2.integration.JaxrsOpenApiContextBuilder()
+                    .openApiConfiguration(config)
+                    .buildContext(true);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+
+        return new HashSet<>();
     }
 }
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -1,5 +1,27 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<!DOCTYPE html>
 <html>
+<head>
+    <title>Fo76 Inventory API</title>
+    <meta charset="utf-8"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" href="https://unpkg.com/swagger-ui-dist/swagger-ui.css">
+</head>
 <body>
-<h2>Hello World!</h2>
+<div id="swagger-ui"></div>
+<script src="https://unpkg.com/swagger-ui-dist/swagger-ui-bundle.js"></script>
+<script>
+    window.onload = function() {
+        SwaggerUIBundle({
+            url: "/InventoryAPI_war/api/openapi.json",
+            dom_id: '#swagger-ui',
+            presets: [
+                SwaggerUIBundle.presets.apis,
+                SwaggerUIBundle.SwaggerUIStandalonePreset
+            ],
+            layout: "BaseLayout"
+        })
+    }
+</script>
 </body>
 </html>
