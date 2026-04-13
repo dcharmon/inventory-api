73eb6c2fb4d3a82b9fa62d76a8bb000d55eac928
dcharmon <dcorbinharmon@gmail.com>
2026-04-13 16:24:20 -0500

    Update split Swagger tags by resource group

--- src/main/java/edu/matc/inventory/rest/InventoryApplication.java
+++ src/main/java/edu/matc/inventory/rest/InventoryApplication.java
@@ -4,10 +4,13 @@ import io.swagger.v3.jaxrs2.integration.resources.OpenApiResource;
 import io.swagger.v3.oas.integration.SwaggerConfiguration;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.tags.Tag;
 import io.swagger.v3.oas.models.servers.Server;
+
 
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.HashSet;
 import java.util.Set;
@@ -30,7 +33,16 @@ public class InventoryApplication extends Application {
                 .info(new Info()
                         .title("Fo76 Inventory API")
                         .version("1.0")
-                        .description("API for managing Fallout 76 player inventories"));
+                        .description("API for managing Fallout 76 player inventories"))
+                .addServersItem(new Server().url("http://localhost:8080/InventoryAPI_war"))
+                .addServersItem(new Server().url("http://inventory-api-fo76-env.eba-kvcd43xn.us-east-2.elasticbeanstalk.com"))
+                .tags(Arrays.asList(
+                        new Tag().name("Armor Types").description("Reference data for FO76 armor types and their base resistances"),
+                        new Tag().name("Legendary Effects").description("Reference data for FO76 legendary effects by star tier"),
+                        new Tag().name("User Armor").description("Manage a user's collected armor pieces"),
+                        new Tag().name("Loadouts").description("Manage a user's named gear loadouts")
+                ));
+
         SwaggerConfiguration config = new SwaggerConfiguration()
                 .openAPI(openAPI)
                 .prettyPrint(true)
